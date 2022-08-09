9b22822e5a702e3fb38d7ea29de926f93ac147ef
Omar Orrantia <omar.o454@gmail.com>
2022-08-08 22:54:58 -0600

    Added js for search an api

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var restBtn = document.getElementById("Restaurants");
+var inputEl = inputEl = document.getElementById("restaurant-search");
+ var searchEl = document.getElementById("search-button");
 
-restBtn.addEventListener("click", );
+ document.getElementById("search-button").addEventListener("click", displaySearch);
 fetch()
+ 
+var apiKey = I7L1ibgINPyP62JknbdhQ6DQoOWwrd9XyiFYAYt1drLIPWt5_gEHkO0a8PHjTJCGZgKXjwtGq0qFgsmh2jplhqzm9t84OexWw9xcc6jpKdt9NXQ3U9pl8sgpiJboYnYx
+// https://api.yelp.com/v3/businesses/search
+// https://api.yelp.com/v3/autocomplete
 
 function myFunction() {
     document.getElementById("Restaurants").classList.toggle("show");
   }
-  
+  console.log(myFunction)
   function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
@@ -21,4 +27,16 @@ function myFunction() {
         a[i].style.display = "none";
       }
     }
+  }
+
+  let autocomplete;
+  function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete'),
+      {
+        types: ['establishments']
+        componentRestictions: {'United States': ['US']}
+        fields: ['place_id', 'geometry', 'name']
+      
+      })
   }
